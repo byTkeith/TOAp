b0d3a633e7bae01d4291f680fc92f76d4ce8ae59
Tendai <youremail@example.com>
2024-09-12 19:43:26 +0200

    used recursion to find the permutations

---
+++ Scoring.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Scoring {
+    public int  ScoreCal(int index,int currentscore,int[] list, int t){
+        if(index==list.length||currentscore>=t){
+            return(currentscore<t)?currentscore:0;
+        }
+        int addScore = ScoreCal(index+1, currentscore+list[index], list, t);
+        int MulScore = ScoreCal(index+1, currentscore*list[index], list, t);
+
+        return Math.max(addScore, MulScore);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your values:\n");
+        int n=sc.nextInt();
+        int[] restOfValues= new int[n];//reads an arraylist the same amount as n
+    }
+}
--- modulo.java
+++ modulo.java
@@ -1,27 +1,17 @@
-import java.util.*;
 public class modulo {
-    int count=0;//count to keep track of values that can pair
-    public int Mod(int n, int z){
-        //Random rm= new Random();
-      
-        for(int x=1;x<=n;x++){
+    public int counter(int n, int z){
+        int count=0;
+        for(int x=1; x<n;x++){
             for(int y=1;y<n;y++){
-                if((x*y)%n==z & n>z & z>=0){
+                if((x*y)%n==z){
                     count++;
                 }
-
             }
-
         }
         return count;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter N and Z on separate lines:\n");
-        int inputN=sc.nextInt();
-        int inputZ=sc.nextInt();
-        modulo md=new modulo();
-        System.out.println(md.Mod(inputN, inputZ));
+        modulo md= new modulo();
+        System.out.println(md.counter(6, 3));
     }
-    
 }
